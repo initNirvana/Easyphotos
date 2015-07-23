143c27a8fd6935231c9a7b1b7e815ae7f1fbc7da
Nirvana <kimqqyun@gmail.com>
2015-07-23 19:22:21 +0900

    dependency check

---
+++ manage.py
@@ -0,0 +1,12 @@
+from flask.ext.script import Manager
+
+from tushe import app
+
+manager = Manager(app)
+
+@manager.command
+def hello():
+        print "hello"
+
+if __name__ == "__main__":
+    manager.run()
